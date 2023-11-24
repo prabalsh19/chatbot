5baf2b71ff0e915488b9198728771c54d8db8047
prabalsh19 <40392568+prabalsh19@users.noreply.github.com>
2023-11-24 15:24:20 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+import { useState } from "react";
+import MetaTags from "react-meta-tags";
+import Fade from "react-reveal/Fade";
+import Flip from "react-reveal/Flip";
+import Chatbot from "react-chatbot-kit";
+import config from "./chatbotConfig";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
+import TypedReact from "./TypedReact";
+
+export default function App() {
+  const [showBot, toggleBot] = useState(false);
+
+  return (
+    <div className="App">
+      <div className="app-chatbot-container">
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
+      </div>
+    </div>
+  );
+}
